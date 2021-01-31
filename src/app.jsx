88272a3b4ffdf78f69375628479730a4d30b29f6
AruSeito <AruSeito@gmail.com>
2021-01-31 15:33:33 +0800

    feat: ➕ 添加webpack配置

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+import "./index.css";
+import "./index.scss";
+import "font-awesome/css/font-awesome.min.css";
+ReactDom.render(
+  <h1>
+    <i className="fa fa-bandcamp"></i>
+    Hello,React22222
+  </h1>,
+  document.querySelector("#app")
+);
